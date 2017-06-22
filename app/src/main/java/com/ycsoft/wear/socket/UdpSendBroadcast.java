3394538ce9f3efdab717e8872c7c78aded18e132
zhangjun_mac <zhangjun713@qq.com>
2017-06-22 11:49:23 +0800

    修改了广播发机制，现在可以往整个局域网中所有地址发广播了，不再限定在255.255.255.0这种掩码对应的子网段了

--- app/src/main/java/com/ycsoft/wear/socket/UdpSendBroadcast.java
+++ app/src/main/java/com/ycsoft/wear/socket/UdpSendBroadcast.java
@@ -3,7 +3,6 @@ package com.ycsoft.wear.socket;
 import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.text.format.Formatter;
 
 import org.xutils.x;
 
@@ -28,34 +27,25 @@ public class UdpSendBroadcast {
         x.task().run(new Runnable() {
             @Override
             public void run() {
-                WifiManager wifiMgr = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                WifiManager wifiMgr = (WifiManager) context.getApplicationContext()
+                        .getSystemService(Context.WIFI_SERVICE);
                 WifiInfo wifiInfo;
                 //这里获取了IP地址，获取到的IP地址还是int类型的
-                int ip;
                 wifiInfo = wifiMgr.getConnectionInfo();
                 if (wifiInfo != null) {//连上了无线网
-                    ip = wifiInfo.getIpAddress();
-                } else {
-                    ip = 0;
-                }
-                //这一步就是将本机的IP地址转换成xxx.xxx.xxx.255
-                if (ip == 0) {
-                    return;
-                }
-                int broadCastIP = ip | 0xFF000000;
-                DatagramSocket theSocket = null;
-                try {
-                    InetAddress server = InetAddress.getByName(Formatter.formatIpAddress(broadCastIP));
-                    theSocket = new DatagramSocket();
-                    DatagramPacket theOutput = new DatagramPacket(message.getBytes(), message.length(), server,
-                            port);
-                    //这一句就是发送广播了，其实255就代表所有的该网段的IP地址，是由路由器完成的工作
-                    theSocket.send(theOutput);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (theSocket != null)
-                        theSocket.close();
+                    DatagramSocket theSocket = null;
+                    try {
+                        InetAddress server = InetAddress.getByName("255.255.255.255");
+                        theSocket = new DatagramSocket();
+                        DatagramPacket theOutput = new DatagramPacket(message.getBytes(), message.length(), server,
+                                port);
+                        theSocket.send(theOutput);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (theSocket != null)
+                            theSocket.close();
+                    }
                 }
             }
         });
